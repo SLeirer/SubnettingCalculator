e1609b8e3bd1fb6f644b6bef77d8bf87c7c9cf41
leirersi <Simon.Leirer@googlemail.com>
2022-10-11 10:30:34 +0200

    Implementationv von OOP

--- SubnettingCalculator/Pages/Index.cshtml.cs
+++ SubnettingCalculator/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using System.Text.RegularExpressions;
 using static System.Formats.Asn1.AsnWriter;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SubnettingCalculator.Pages
 {
@@ -15,17 +16,9 @@ namespace SubnettingCalculator.Pages
         {
             _logger = logger;
         }
-
-        public string ErsterHost { get; set; }
-        public string SubnetzMaske { get; set; }
-        public string LetzterHost { get; set; }
-        public string Broadcast { get; set; }
-        public string AnzahlHosts { get; set; }
-        public string NetzAnteil { get; set; }
-        public string NetID { get; set; }
         public string Message { get; set; }
 
-
+        public List<SubnetzObject> subnetzliste = new List<SubnetzObject>();
 
         [BindProperty]
         public string NetIdEingabe { get; set; }
@@ -39,125 +32,17 @@ namespace SubnettingCalculator.Pages
         }
 
         public void OnPost()
-        {            
-            if(!isValidInput(NetIdEingabe, NetzAnteilEingabe))
+        {
+            if (!isValidInput(NetIdEingabe, NetzAnteilEingabe))
             {
                 return;
             }
-            NetID = NetIdEingabe;
-            NetzAnteil = NetzAnteilEingabe;
-            SubnetzMaske = evaluateSubnetmask(NetzAnteil);
-            ErsterHost = evaluateFirstHost(NetID, NetzAnteil);
-            AnzahlHosts = (Math.Pow(2, 32 - Convert.ToInt32(NetzAnteil))-2).ToString();
-            if(Convert.ToInt32(AnzahlHosts) < 2)
-            {
-                AnzahlHosts = (Convert.ToInt32(AnzahlHosts) + 2).ToString();
-            }
-
-            Broadcast = evaluateBroadcast(NetzAnteil, NetIdEingabe);
-
-        }
-
-        public string evaluateBroadcast(string NetzAnteil, string NetID)
-        {
-            string broadcastStr = "";
-            if (NetzAnteil == "32")
-            {
-                return NetID;
-            }
-            if(NetzAnteil == "31")
-            {
-                broadcastStr = NetID.Substring(NetID.LastIndexOf('.')+1);
-                broadcastStr = (Convert.ToInt32(broadcastStr) + 1).ToString();
-                broadcastStr = NetID.Substring(0, NetID.LastIndexOf('.')+1) + broadcastStr;
-                return broadcastStr;
-            }
-            
-            int hostanteile = 32 - Convert.ToInt32(NetzAnteil);
-            int ganzeAchterblocks = hostanteile / 8;
-            int temp = 1;
-            int erg = 0;
-            string[] netIDblocks = NetID.Split('.');
-            int[] netIDblocksInt = new int[4];
-
-          
-
-            for (int i=0; i<netIDblocks.Length; i++)
-            {
-                netIDblocksInt[i] = Convert.ToInt32(netIDblocks[i]);
-            }
-
-            hostanteile = hostanteile - ganzeAchterblocks * 8;
-            for(int i = 0; i < hostanteile; i++)
-            {
-                erg += temp;
-                temp = temp * 2;
-            }
-
-            for(int i = 0; i < ganzeAchterblocks; i++)
-            {
-                netIDblocksInt[3 - i] = 255;
-            }
-            
-            netIDblocksInt[3 - ganzeAchterblocks] = netIDblocksInt[3 - ganzeAchterblocks] + erg;
-
-            foreach(int block in netIDblocksInt)
-            {
-                broadcastStr += block.ToString() + ".";
-            }
-            broadcastStr = broadcastStr.Substring(0, broadcastStr.Length-1);
-
-            return broadcastStr;
-        }
-        public string evaluateFirstHost(string NetID, string NetzAnteil)
-        {
-            if(NetzAnteil == "31" || NetzAnteil == "32")
-            {
-                return NetID;
-            }
-            string firstHost = "";
-            string NetIDlastBlock = NetID.Substring(NetID.LastIndexOf('.')+1);
-            int temp = Convert.ToInt32(NetIDlastBlock);
-            temp += 1;
-            firstHost = NetID.Substring(0, NetID.Length-NetIDlastBlock.Length);
-            firstHost += temp.ToString();
-
-            return firstHost;
-        }
-        public string evaluateSubnetmask(string NetzAnteil)
-        {
-            string subnetzmaske = "";
-            int anzahlNetzbits = Convert.ToInt32(NetzAnteil);
-            int volleAchterBlocks = anzahlNetzbits / 8;
-            int uebrigeBits = anzahlNetzbits - volleAchterBlocks*8;
-            int temp = 128;
-            int erg = 0;
-
-            for(int i = 0; i < volleAchterBlocks; i++)
-            {
-                subnetzmaske += "255.";
-            }
-            
-            for(int i = 0; i < uebrigeBits; i++)
-            {
-                erg += temp;
-                temp = temp / 2;
-            }
-            subnetzmaske += erg + ".";
-
-            while(subnetzmaske.Count(f => f == '.') < 4)
-            {
-                subnetzmaske += "0.";
-            }
-
-            subnetzmaske = subnetzmaske.Substring(0, subnetzmaske.Length-1);
-            if(subnetzmaske.Length > 15)
-            {
-                subnetzmaske = subnetzmaske.Substring(0, 15);
-            }
-
-
-            return subnetzmaske;
+            SubnetzObject erstesSubnetz = new();
+            erstesSubnetz.initializeSubnet(NetIdEingabe, NetzAnteilEingabe);
+            subnetzliste.Add(erstesSubnetz);
+            subnetzliste.Add(erstesSubnetz);
+            subnetzliste.Add(erstesSubnetz);
+            subnetzliste.Add(erstesSubnetz);
         }
         public Boolean isValidInput(string NetIdEingabe, string NetzAnteilEingabe)
         {
@@ -221,24 +106,16 @@ namespace SubnettingCalculator.Pages
 
             //CHECK IF NETID AND SUBNET FIT TOGETHER DEPENDING OF NUMBER OF HOSTS
             //irgendwas ist hier noch nicht richtig
-            int localHostBits = 32 - Convert.ToInt32(NetzAnteilEingabe);
-            int anzahlAchterBloecke = localHostBits / 8;
-            if(anzahlAchterBloecke > 0)
+            double hosts = Math.Pow(2, 32 - Convert.ToInt32(NetzAnteilEingabe));
+            double hostPlatzImSubnetz = 1;
+            for(int i = 0; i < netIdBloackeInteger.Length; i++)
             {
-                localHostBits = localHostBits - anzahlAchterBloecke * 8;
+                hostPlatzImSubnetz = hostPlatzImSubnetz + (255-netIdBloackeInteger[i])*(Math.Pow(255, 3-i));
             }
-            if (netIdBloackeInteger[3-anzahlAchterBloecke] > Math.Pow(2, localHostBits))
+            if(hosts > hostPlatzImSubnetz)
             {
-                Message = "Nicht genügend IP's für diesese subnetzt";
+                Message = "netID hat nicht genügend hosts für dieses subnetz";
                 return false;
-            }
-            for(int i = 0; i < anzahlAchterBloecke; i++)
-            {
-                if (netIdBloackeInteger[3 - anzahlAchterBloecke] == Math.Pow(2, localHostBits) && netIdBloackeInteger[3-i] > 0)
-                {
-                    Message = "Nicht genügend IP's für diesese subnetzt";
-                    return false;
-                }
             }
 
 
---
+++ SubnettingCalculator/Pages/SubnetzObject.cs
@@ -0,0 +1,143 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SubnettingCalculator.Pages
+{
+    public class SubnetzObject
+    {
+        public string ErsterHost { get; set; }
+        public string SubnetzMaske { get; set; }
+        public string LetzterHost { get; set; }
+        public string Broadcast { get; set; }
+        public string AnzahlHosts { get; set; }
+        public string NetzAnteil { get; set; }
+        public string NetID { get; set; }
+        public string Message { get; set; }
+
+        public SubnetzObject()
+        {
+            this.ErsterHost = "";
+            this.SubnetzMaske = "";
+            this.LetzterHost = "";
+            this.Broadcast = "";
+            this.AnzahlHosts = "";
+            this.NetzAnteil = "";
+            this.NetID = "";
+            this.Message = "";
+        }
+
+        public string evaluateBroadcast(string NetzAnteil, string NetID)
+        {
+            string broadcastStr = "";
+            if (NetzAnteil == "32")
+            {
+                return NetID;
+            }
+            if (NetzAnteil == "31")
+            {
+                broadcastStr = NetID.Substring(NetID.LastIndexOf('.') + 1);
+                broadcastStr = (Convert.ToInt32(broadcastStr) + 1).ToString();
+                broadcastStr = NetID.Substring(0, NetID.LastIndexOf('.') + 1) + broadcastStr;
+                return broadcastStr;
+            }
+
+            int hostanteile = 32 - Convert.ToInt32(NetzAnteil);
+            int ganzeAchterblocks = hostanteile / 8;
+            int temp = 1;
+            int erg = 0;
+            string[] netIDblocks = NetID.Split('.');
+            int[] netIDblocksInt = new int[4];
+
+
+
+            for (int i = 0; i < netIDblocks.Length; i++)
+            {
+                netIDblocksInt[i] = Convert.ToInt32(netIDblocks[i]);
+            }
+
+            hostanteile = hostanteile - ganzeAchterblocks * 8;
+            for (int i = 0; i < hostanteile; i++)
+            {
+                erg += temp;
+                temp = temp * 2;
+            }
+
+            for (int i = 0; i < ganzeAchterblocks; i++)
+            {
+                netIDblocksInt[3 - i] = 255;
+            }
+
+            netIDblocksInt[3 - ganzeAchterblocks] = netIDblocksInt[3 - ganzeAchterblocks] + erg;
+
+            foreach (int block in netIDblocksInt)
+            {
+                broadcastStr += block.ToString() + ".";
+            }
+            broadcastStr = broadcastStr.Substring(0, broadcastStr.Length - 1);
+
+            return broadcastStr;
+        }
+        public string evaluateFirstHost(string NetID, string NetzAnteil)
+        {
+            if (NetzAnteil == "31" || NetzAnteil == "32")
+            {
+                return NetID;
+            }
+            string firstHost = "";
+            string NetIDlastBlock = NetID.Substring(NetID.LastIndexOf('.') + 1);
+            int temp = Convert.ToInt32(NetIDlastBlock);
+            temp += 1;
+            firstHost = NetID.Substring(0, NetID.Length - NetIDlastBlock.Length);
+            firstHost += temp.ToString();
+
+            return firstHost;
+        }
+        public string evaluateSubnetmask(string NetzAnteil)
+        {
+            string subnetzmaske = "";
+            int anzahlNetzbits = Convert.ToInt32(NetzAnteil);
+            int volleAchterBlocks = anzahlNetzbits / 8;
+            int uebrigeBits = anzahlNetzbits - volleAchterBlocks * 8;
+            int temp = 128;
+            int erg = 0;
+
+            for (int i = 0; i < volleAchterBlocks; i++)
+            {
+                subnetzmaske += "255.";
+            }
+
+            for (int i = 0; i < uebrigeBits; i++)
+            {
+                erg += temp;
+                temp = temp / 2;
+            }
+            subnetzmaske += erg + ".";
+
+            while (subnetzmaske.Count(f => f == '.') < 4)
+            {
+                subnetzmaske += "0.";
+            }
+
+            subnetzmaske = subnetzmaske.Substring(0, subnetzmaske.Length - 1);
+            if (subnetzmaske.Length > 15)
+            {
+                subnetzmaske = subnetzmaske.Substring(0, 15);
+            }
+
+
+            return subnetzmaske;
+        }
+        public void initializeSubnet(string netIDEingabe, string netzAnteilEingabe)
+        {
+            this.NetID = netIDEingabe;
+            this.NetzAnteil = netzAnteilEingabe;
+            this.AnzahlHosts = (Math.Pow(2, 32 - Convert.ToInt32(this.NetzAnteil)) - 2).ToString();
+            if (Convert.ToInt32(this.AnzahlHosts) < 2)
+            {
+                this.AnzahlHosts = (Convert.ToInt32(this.AnzahlHosts) + 2).ToString();
+            }
+            this.SubnetzMaske = evaluateSubnetmask(NetzAnteil);
+            this.ErsterHost = evaluateFirstHost(NetID, NetzAnteil);
+            this.Broadcast = evaluateBroadcast(NetzAnteil, NetID);
+        }
+    }
+}
